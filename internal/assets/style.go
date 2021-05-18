69a3f8c31e4a2b76fb28e20c68d264e1960bbded
Yongbin Kim <iam@yongbin.kim>
2021-05-18 12:42:18 +0900

    style handler

---
+++ internal/assets/style.go
@@ -0,0 +1,16 @@
+package assets
+
+import (
+	"net/http"
+
+	_ "embed"
+)
+
+//go:embed style.css
+var styleCSS []byte
+
+func StyleHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/css;charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write(styleCSS)
+}
