9f235a6dc983eeb905b91707c5d905902fde6796
Yongbin Kim <iam@yongbin.kim>
2021-05-20 21:21:56 +0900

    templates: Package added

---
+++ internal/tpls/tpls.go
@@ -0,0 +1,100 @@
+package tpls
+
+import (
+	"embed"
+	"errors"
+	"fmt"
+	"html/template"
+	"io"
+	"io/fs"
+	"path/filepath"
+)
+
+//go:embed html/*.html
+var tplfs embed.FS
+
+var ErrTemplateNotFound = errors.New("tpls: template not found")
+
+type Template struct {
+	templates map[string]*template.Template
+}
+
+func readTpls(tplfs fs.ReadDirFS) (*Template, error) {
+	baseTpl := template.New("")
+
+	// Get the files that need to be read from the directory
+	files, err := tplfs.ReadDir("html")
+	if err != nil {
+		return nil, err
+	}
+
+	// Add files starting with "_" to the base template and
+	// exclude them from the file list.
+	for i := len(files) - 1; i >= 0; i-- {
+		file := files[i]
+		fileName := file.Name()
+		if fileName[0] != '_' {
+			continue
+		}
+
+		_, err = parseTpl(baseTpl, tplfs, filepath.Join("html/", fileName))
+		if err != nil {
+			return nil, err
+		}
+
+		files[i] = files[len(files)-1]
+		files = files[:len(files)-1]
+		break
+	}
+
+	// Template map is here...
+	var tpls = make(map[string]*template.Template)
+
+	// Parse the files and add them to the template map.
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		var fileName = file.Name()
+
+		tpl, err := baseTpl.Clone()
+		if err != nil {
+			return nil, err
+		}
+		tpl = tpl.New(fileName)
+
+		_, err = parseTpl(tpl, tplfs, filepath.Join("html/", fileName))
+		if err != nil {
+			return nil, err
+		}
+
+		tpls[fileName] = tpl
+	}
+
+	return &Template{templates: tpls}, nil
+}
+
+func parseTpl(t *template.Template, fs fs.FS, name string) (*template.Template, error) {
+	file, err := fs.Open(name)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Print(string(data))
+
+	return t.Parse(string(data))
+}
+
+func (t *Template) ExecuteTemplate(w io.Writer, name string, data interface{}) error {
+	tpl, ok := t.templates[name]
+	if !ok {
+		return ErrTemplateNotFound
+	}
+	return tpl.Execute(w, data)
+}
---
+++ internal/tpls/tpls_test.go
@@ -0,0 +1,51 @@
+package tpls
+
+import (
+	"bytes"
+	"testing"
+	"testing/fstest"
+)
+
+func TestFS(t *testing.T) {
+	err := fstest.TestFS(tplfs, "html/_template.html", "html/index.html")
+	if err != nil {
+		t.Errorf("looks like the required files in the template directory are missing: %s", err)
+	}
+}
+
+var fakeTpls = fstest.MapFS{
+	"html/_template.html": &fstest.MapFile{
+		Data: []byte(`{{define "template"}}<template>{{block "contents" .}}No Contents!{{end}}</template>{{end}}`),
+	},
+	"html/index.html": &fstest.MapFile{
+		Data: []byte(`{{define "contents"}}Hello, World!{{end}}{{template "template" .}}`),
+	},
+}
+
+const expectedTemplateResult = "<template>Hello, World!</template>"
+
+func TestReadTpls(t *testing.T) {
+	tpl, err := readTpls(fakeTpls)
+	if err != nil {
+		t.Errorf("readTpls returned an error: %s", err)
+		return
+	}
+
+	buf := new(bytes.Buffer)
+
+	err = tpl.ExecuteTemplate(buf, "???", nil)
+	if err != ErrTemplateNotFound {
+		t.Errorf("ExecuteTemplate throws an error: %s, want: %s", err, ErrTemplateNotFound)
+	}
+
+	err = tpl.ExecuteTemplate(buf, "index.html", nil)
+	if err != nil {
+		t.Errorf("ExecuteTemplate returned an error: %s", err)
+		return
+	}
+
+	if !bytes.Equal(buf.Bytes(), []byte(expectedTemplateResult)) {
+		t.Errorf("ExecuteTemplate returned wrong result: %s, want: %s", buf.String(), expectedTemplateResult)
+		return
+	}
+}
