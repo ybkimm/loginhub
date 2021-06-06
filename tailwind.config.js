9ad57bd556e5dd943c6ad729adbf4a3a4065ae00
Yongbin Kim <iam@yongbin.kim>
2021-06-06 18:03:05 +0900

    styles: Add some tailwind variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,41 @@ module.exports = {
   theme: {
     extend: {
       lineHeight: (() => {
-        let map = {}
+        /**
+         * @type {Record<number, string>}
+         */
+        const map = {}
         for (let i = 11; i <= 30; i++) {
-          map[i] = `${i*0.25}rem`
+          map[i] = `${i * 0.25}rem`
         }
         return map
-      })()
-    },
+      })(),
+      colors: {
+        primary: {
+          light: '#b2f1ad',
+          DEFAULT: '#71e46d',
+          dark: '#1eb83f'
+        },
+        secondary: {
+          light: '#f2c3f3',
+          DEFAULT: '#e06de4',
+          dark: '#a716c6'
+        }
+      },
+      spacing: {
+        login: '2.33rem'
+      },
+      fontSize: {
+        login: ['1rem', '2.33rem']
+      }
+    }
   },
-  variants: {},
-  plugins: []
+  variants: {
+    extend: {
+      margin: ['last']
+    }
+  },
+  plugins: [
+    require('@tailwindcss/forms')
+  ]
 }
